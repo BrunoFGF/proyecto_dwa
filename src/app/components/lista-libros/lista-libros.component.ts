4c3012fbf6dff15ba82046bc4f6732f60de10d40
LUIS ALEXANDER AGUIRRE SALDIVIA <luis.aguirresal@ug.edu.ec>
2024-11-19 09:45:08 -0500

    a

--- src/app/components/lista-libros/lista-libros.component.ts
+++ src/app/components/lista-libros/lista-libros.component.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { Libros } from '../../models/Libros';
+import { LibrosjsonService } from '../../services/librosjson.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogData, MyDialogComponent } from '../shared/my-dialog/my-dialog.component';
+import { UpperCasePipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-lista-libros',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './lista-libros.component.html',
   styleUrl: './lista-libros.component.css'
 })
 export class ListaLibrosComponent {
 
+  title= "Lista de peliculas";
+  libros: Libros[]=[];
+
+  constructor(private miServicio:LibrosjsonService, private mydialog:MatDialog){}
+
+  ngOnInit():void{this.cargarLibros();};
+
+  cargarLibros():void{
+
+    this.miServicio.getLibros().subscribe((data:Libros[])=>{
+      this.libros = data;
+      console.log(this.libros[0]);
+    });
+
+  }
+
+  intercambiar(libro:Libros):void{
+    const dialogRef = this.mydialog.open(MyDialogComponent,{
+
+      data:{
+        titulo:libro.titulo,
+        contenido: "El libro" + libro.titulo + " entro en proceso de intercambio"
+      }as DialogData,
+    } );//para abrir la ventana de dialogo
+    dialogRef.afterClosed().subscribe(result =>{
+      if(result === "aceptar"){
+        console.log("Aceptar");
+      }else if (result === "cancelar"){
+        console.error("Cancelar"); 
+      }
+    });
+
+  }
+
+  activar(imgLibro:HTMLImageElement){
+
+    imgLibro.classList.add("activa")
+  }
+  
+  desactivar(imgLibro:HTMLImageElement){
+
+    imgLibro.classList.remove("activa")
+  }
+
+
+
+
 }
+
--- src/app/services/librosjson.service.ts
+++ src/app/services/librosjson.service.ts
@@ -1,9 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Libros } from '../models/Libros';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LibrosjsonService {
+  private jsonUrl = 'http://localhost:3000/libros';
+  constructor(private http: HttpClient) {}
+  //metodo
 
-  constructor() { }
+  getLibros(): Observable<Libros[]> {
+    return this.http.get<Libros[]>(this.jsonUrl);
+  }
+
+  ///Buscar
+  getLibrosBusqueda(area?: string, categoria?: string): Observable<Libros[]> {
+    return this.http.get<Libros[]>(this.jsonUrl).pipe(
+      map((libros)=>
+        libros.filter((libros)=>
+        (area? libros.area.toLowerCase().includes(area.toLowerCase()):true) &&
+        (categoria? libros.categoria.toLowerCase().includes(categoria.toLowerCase()):true)
+    )
+    )
+    );
+  }
+
+  ///Agregar
+  addMovie(libros: Libros): Observable<Libros> {
+    return this.http.post<Libros>(this.jsonUrl, libros);
+  }
+
+  ///Editar
+
+  updateMovie(libros: Libros): Observable<Libros> {
+    const urlDeLosLibros = `${this.jsonUrl}/${libros.id}`;
+    return this.http.put<Libros>(urlDeLosLibros, libros);
+  }
+
+  //ELiminar
+
+  deleteMovie(libros: Libros): Observable<void> {
+    const urlDeLosLibros = `${this.jsonUrl}/${libros.id}`;
+    return this.http.delete<void>(urlDeLosLibros);
+  }
 }

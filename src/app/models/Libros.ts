23ac12d9b856df36586752d4749ee1bb2d65b6de
LUIS ALEXANDER AGUIRRE SALDIVIA <luis.aguirresal@ug.edu.ec>
2024-11-19 08:40:37 -0500

    a

---
+++ src/app/models/Libros.ts
@@ -0,0 +1,14 @@
+export interface Libros{
+    id?: number;
+    titulo: string;
+    autor: string;
+    area: string;
+    categoria: string;
+    estado: string;
+    descripcion: string;
+    idioma: string;
+    ubicacion: string;
+    imagen: string;
+    disponibilidad: boolean;
+    reseñas: string;
+}

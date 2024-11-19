174e29531ee38778e8913c89b74ac817a586c142
LUIS ALEXANDER AGUIRRE SALDIVIA <luis.aguirresal@ug.edu.ec>
2024-11-19 17:00:33 -0500

    grande

--- src/app/components/crud-libros/crud-libros.component.ts
+++ src/app/components/crud-libros/crud-libros.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { LibrosjsonService } from '../../services/librosjson.service';
 import { Libros } from '../../models/Libros';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatDialog } from '@angular/material/dialog';
@@ -41,17 +41,16 @@ export class CrudLibrosComponent implements OnInit, AfterViewInit{
 
   //inicializar las variables asociadas a los componentes del formulario
   this.form=this.fb.group({
-    titulo: [""],
-    autor: [""],
-    area: [""],
-    categoria: [""],
-    estado: [""],
-    descripcion: [""],
-    idioma: [""],
-    ubicacion: [""],
+    titulo: ["", [Validators.required, Validators.minLength(3), Validators.pattern(/[A-Za-z0-9]+/g)]],
+    autor: ["",[Validators.required]],
+    area: ["",[Validators.required]],
+    categoria: ["",[Validators.required]],
+    estado: ["",[Validators.required]],
+    descripcion: ["",[Validators.required]],
+    idioma: ["",[Validators.required]],
+    ubicacion: ["",[Validators.required]],
     imagen: [""],
-    disponibilidad: [""],
-    reseñas: [""]
+    disponibilidad: [false]
   });
   }
 
@@ -77,7 +76,26 @@ export class CrudLibrosComponent implements OnInit, AfterViewInit{
   }
 
   editar(libros:Libros):void{
+    this.isEditMode = true;
 
+    if(libros && libros.id){
+      this.currentId = libros.id;
+    }else{
+      console.log("Libro o el id del libro estan undefined")
+    }
+    ///cargamos los datos de la peli en 
+    this.form.setValue({
+      titulo: libros.titulo,
+      autor: libros.autor,
+      area: libros.area,
+      categoria: libros.categoria,
+      estado: libros.estado,
+      descripcion: libros.descripcion ,
+      idioma: libros.idioma,
+      ubicacion: libros.ubicacion,
+      imagen: libros.imagen?libros.imagen:'',
+      disponibilidad: libros.disponibilidad
+    })
   }
 
   eliminar(libros:Libros):void{
@@ -99,10 +117,55 @@ export class CrudLibrosComponent implements OnInit, AfterViewInit{
     });
   }
 
+  clearForm():void{
+    this.form.reset({
+      titulo: '',
+      autor: '',
+      area: '',
+      categoria: '',
+      estado: '',
+      descripcion: '',
+      idioma: '',
+      ubicacion: '',
+      imagen: '',
+      disponibilidad: false,
+    });
+    this.currentId=0;
+    this.isEditMode = false;
+  }
 
-  onSubmit(){}
+  onSubmit(){
 
-  areas: string[] = ['Literatura', 'Ciencia', 'Arte']; // Áreas de ejemplo
+    //revisar si el formulario es valido
+    if(this.form.invalid){
+      return;
+    }
+
+    //obtener los datos de los controles del formulario
+
+    const newLibro:Libros=this.form.value;
+    if(this.isEditMode){//editar
+
+      newLibro.id = this.currentId;
+      this.librosService.updateLibro(newLibro).subscribe((updateLibro)=>{
+        alert("El libro fue editado exitosamente");
+        this.getLibros();
+      });
+
+    }else{//agregar
+
+      this.librosService.addLibro(newLibro).subscribe((addLibro)=>{
+        alert("El libro fue agregado exitosamente");
+        this.getLibros();
+      });
+    }
+
+    this.clearForm();
+
+  }
+
+  //seleccionar Area y mostrar categoria segun el area.
+  areas: string[] = ['Literatura', 'Ciencia', 'Arte']; // Áreas
   categorias: string[] = [];
   selectedArea: string = '';
   selectedCategoria: string = '';

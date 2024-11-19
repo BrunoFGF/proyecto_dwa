a8fb6fd01238504faeac8274b12d8912ceb581c4
LUIS ALEXANDER AGUIRRE SALDIVIA <luis.aguirresal@ug.edu.ec>
2024-11-19 10:20:44 -0500

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListaLibrosComponent } from './components/lista-libros/lista-libros.component';
+import { CrudLibrosComponent } from './components/crud-libros/crud-libros.component';
 
 export const routes: Routes = [
     {path: "lista-libros", component: ListaLibrosComponent},
+    {path: "crud-libros", component: CrudLibrosComponent},
     {path: "", redirectTo:"lista-libros", pathMatch:'full'},
     {path: "**", redirectTo:"lista-libros"},///**  otra ruta mal */
 

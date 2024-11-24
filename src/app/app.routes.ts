cec704e582853f70c25611f90dc32c9a4df13dbe
ELIUD ALFONSO SUAREZ CLEMENTE <eliud.suarezcle@ug.edu.ec>
2024-11-24 08:14:09 -0500

    comp main - comp resenia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { ListaLibrosComponent } from './components/lista-libros/lista-libros.com
 import { CrudLibrosComponent } from './components/crud-libros/crud-libros.component';
 import { ListaAutoresComponent } from './components/lista-autores/lista-autores.component';
 import { CrudAutoresComponent } from './components/crud-autores/crud-autores.component';
+import { ListaReseniaComponent } from './components/lista-resenia/lista-resenia.component';
 
 export const routes: Routes = [
     {path: "lista-libros", component: ListaLibrosComponent},
     {path: "crud-libros", component: CrudLibrosComponent},
     {path: "lista-autores", component: ListaAutoresComponent},
     {path: "crud-autores", component: CrudAutoresComponent},
+    {path: "lista-resenia",component: ListaReseniaComponent},
     {path: "", redirectTo:"lista-libros", pathMatch:'full'},
     {path: "**", redirectTo:"lista-libros"},///**  otra ruta mal */
 

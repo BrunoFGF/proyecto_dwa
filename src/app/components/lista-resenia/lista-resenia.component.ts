9d6c2ee06f9cadaa88fa484cbcd468ca32ae3aa2
ELIUD ALFONSO SUAREZ CLEMENTE <eliud.suarezcle@ug.edu.ec>
2024-11-24 08:07:59 -0500

    componente lista-resenia

--- src/app/components/lista-resenia/lista-resenia.component.ts
+++ src/app/components/lista-resenia/lista-resenia.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { ReseniajsonService } from '../../services/reseniajson.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MyDialogComponent, DialogData } from '../shared/my-dialog/my-dialog.component';
+import { Resenia } from '../../models/Resenia';
+import { NgFor } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-lista-resenia',
   standalone: true,
-  imports: [],
+  imports: [NgFor,MatCardModule],
   templateUrl: './lista-resenia.component.html',
   styleUrl: './lista-resenia.component.css'
 })
-export class ListaReseniaComponent {
+export class ListaReseniaComponent implements OnInit {
+  resenias: Resenia[] = [];
+  title = 'Lista de Reseñas';
+
+  constructor(private reseniajsonService: ReseniajsonService, private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.cargarResenias();
+  }
+
+  cargarResenias(): void {
+    this.reseniajsonService.getResenias().subscribe((data) => (this.resenias = data));
+  }
+
+  intercambiar(resenia: Resenia): void {
+    const dialogRef = this.dialog.open(MyDialogComponent, {
+      data: {
+        titulo: 'Intercambio de Reseña',
+        contenido: `¿Deseas intercambiar la reseña "${resenia.titulo}"?`,
+      } as DialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'aceptar') {
+        console.log('Intercambio aceptado');
+      } else {
+        console.log('Intercambio cancelado');
+      }
+    });
+  }
 
 }
+

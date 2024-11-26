2f02a5700edb68699f265b985e48d34067d3d145
ELIUD ALFONSO SUAREZ CLEMENTE <eliud.suarezcle@ug.edu.ec>
2024-11-25 19:26:21 -0500

    mejora-diseño

--- src/app/components/lista-resenia/lista-resenia.component.ts
+++ src/app/components/lista-resenia/lista-resenia.component.ts
@@ -1,50 +1,35 @@
-
 import { Component, OnInit } from '@angular/core';
 import { ReseniajsonService } from '../../services/reseniajson.service';
 import { MatDialog } from '@angular/material/dialog';
-import { MyDialogComponent, DialogData } from '../shared/my-dialog/my-dialog.component';
 import { Resenia } from '../../models/Resenia';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-lista-resenia',
   standalone: true,
-  imports: [NgFor,MatCardModule],
+  imports: [NgFor, NgIf, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './lista-resenia.component.html',
   styleUrl: './lista-resenia.component.css'
 })
-export class ListaReseniaComponent {
+export class ListaReseniaComponent implements OnInit {
   
   title = 'Lista de Reseñas';
   resenias: Resenia[] = [];
 
   constructor(private miServicio: ReseniajsonService, private mydialog: MatDialog) {}
 
-  ngOnInit(): void {this.cargarResenias();};
+  ngOnInit(): void {
+    this.cargarResenias();
+  }
 
   cargarResenias(): void {
-    this.miServicio.getResenias().subscribe((data:Resenia[]) =>{
-      this.resenias=data;
-      console.log(this.resenias[1]);
+    this.miServicio.getResenias().subscribe((data:Resenia[]) => {
+      this.resenias = data;
     }) 
   }
 
-  intercambiar(resenias: Resenia): void {
-    const dialogRef = this.mydialog.open(MyDialogComponent, {
-      data: {
-        titulo: 'Intercambio de Reseña',
-        contenido: `¿Te gusto "${resenias.titulo}"?`,
-      } as DialogData,
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result === 'aceptar') {
-        console.log('Intercambio aceptado');
-      } else {
-        console.log('Intercambio cancelado');
-      }
-    });
-  }
-
+  
 }
-
